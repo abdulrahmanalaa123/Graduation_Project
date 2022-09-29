860a43e2a741f3aeb12f5419b77e5d574134632c
abdulrahmanalaa123 <abdulrahmanalaa497@gmail.com>
2022-09-29 09:51:06 +0200

    Update YAAD_self_annotation.py

--- YAAD_self_annotation.py
+++ YAAD_self_annotation.py
@@ -17,33 +17,33 @@ df = pd.DataFrame(array.reshape(1,-1))
 for paths in matfiles:
     mat = scipy.io.loadmat(paths)
     temp = pd.DataFrame(mat["ECGdata"])
-    temp["Session ID"] = int(re.search("(s)\w{1}",paths[-11:])[0][1])
-    temp["Video ID"] = int(re.search("(v)\w{1}",paths[-11:])[0][1])
-    temp["Participant ID"] = int("".join(re.findall("\d",re.search("(p)\w{2}",paths[-11:])[0])))
-    df = df.append(temp)
+    temp["Session ID"] = re.search("(s)\w{1}",paths[-11:])[0][1]
+    temp["Video ID"] = re.search("(v)\w{1}",paths[-11:])[0][1]
+    temp["Participant ID"] = "".join(re.findall("\d",re.search("(p)\w{2}",paths[-11:])[0]))
+    df = df.append(temp,ignore_index = True)
 
-df = df.iloc[1:,:]
-
+df = df.iloc[1:,:].reset_index()
 
 self_data = pd.read_csv("E:/ABDO/Graduation project/Datasets/Kaggle/g2p7vwxyn2-1/ECG_GSR_Emotions/Self-Annotation Labels/Single_modal(encoded).csv")
 #self_data = self_data.astype({"Arousal level": int})
 cols = ['Happy','Sad', 'Fear', 'Anger', 'Neutral', 'Disgust', 'Surprised']
 self_data = self_data.rename(columns = {"Participant Id":"Participant ID","Session Id":"Session ID","Video Id":"Video ID"})
-print(self_data.columns)
+print(self_data)
 identifiers = ["Participant ID","Session ID","Video ID"]
+df = df.astype(dict(zip(identifiers,[int,int,int])))
+print(df.columns)
 tuples = list(map(tuple,self_data[identifiers].values))
 values = self_data[cols].values
 mapper = dict(zip(tuples,values))
-print(mapper)
 
-df[cols] = pd.DataFrame(list(map(lambda x:mapper[x],list(map(tuple,(temp[identifiers].values))))))
-#the 5000 samples aarent ordered are actually selected randomly(i think) from 10000 values
+
+mapped = pd.DataFrame(list(map(lambda x:mapper[x],list(map(tuple,(df[identifiers].values))))))
+print(mapped)
+df[cols] = mapped
+
+
 timestamps = np.arange(0,39,1/256)
 renamedict = dict(zip(array,timestamps))
 df = df.rename(columns = renamedict)
 
 df.to_csv(f"{path}/concatenated.csv")
-
-
-
-    
