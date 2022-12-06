d75e92ff48c0a20c3499898644f6a97f2a9bd56d
abdulrahmanalaa123 <abdulrahmanalaa497@gmail.com>
2022-12-06 09:06:16 +0200

    Update 1dcnn.py

--- 1dcnn.py
+++ 1dcnn.py
@@ -1,17 +1,24 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-from Datacheck import sampler,hrdata,encoder
+from Datacheck import sampler,splittuple,encoder
 from sklearn.preprocessing import normalize
 
 
 if __name__ == "__main__":
     data = pd.read_pickle("E:/ABDO/Graduation project/Datasets/Emognition/hr(tuples).pkl")
     data["Target"] = encoder(data,"Target")
-    hrdat = hrdata(data)
+    hrdat = splittuple(data,0)
+    print(data)
+    #code for including ppinterval is commented in case we dont need it acc 66%
+    #ppinterval = splittuple(data,1)
     X_train,X_test,train_target,test_target = sampler(hrdat)
+    #X_train2,X_test2,_,_ = sampler(ppinterval)
+    #X_train = np.dstack((X_train,X_train2))
+    #X_test = np.dstack((X_test,X_test2))
+
     #X_train = np.asarray(X_train).astype("float32")
-    X_test = np.asarray(X_test).astype("float32")
+    #X_test = np.asarray(X_test3).astype("float32")
     print(X_train.shape)
     print(X_test.shape)
     print(train_target.shape)
@@ -21,14 +28,14 @@ if __name__ == "__main__":
     model.add(tf.keras.layers.Conv1D(filters = 64,kernel_size=3,activation="relu",input_shape = (X_train.shape[1],1)))
     model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=3, activation='relu'))
     model.add(tf.keras.layers.Dropout(0.5))
-    model.add(tf.keras.layers.AveragePooling1D(pool_size=2))
+    model.add(tf.keras.layers.MaxPooling1D(pool_size=2))
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(units = 100,activation="relu"))
     model.add(tf.keras.layers.Dense(11,activation="softmax"))
     print(model.summary())
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     history = model.fit(X_train,train_target,validation_split = 0.2
-        ,batch_size = 32,epochs = 50
+        ,batch_size = 42,epochs = 50
         ,callbacks = [
             tf.keras.callbacks.EarlyStopping(
                 monitor = "val_loss",
